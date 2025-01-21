e8102f0024e429da945afb305955ade1ff47b8e1
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-01-21 11:38:32 -0500

    Implementado un slider con animaciones de desplazamiento continuo y bucle infinito.

--- components/BreakingNews.tsx
+++ components/BreakingNews.tsx
@@ -1,11 +1,13 @@
-import React, { useRef, useState } from "react";
-import { StyleSheet, Text, View, ViewToken } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, Text, useWindowDimensions, View, ViewToken } from "react-native";
 import { Colors } from "@/constants/Colors";
 import { NewsDataType } from "@/types";
 import SliderItem from "@/components/SliderItem";
 import Animated, {
+  scrollTo,
   useAnimatedRef,
   useAnimatedScrollHandler,
+  useDerivedValue,
   useSharedValue,
 } from "react-native-reanimated";
 import Pagination from "@/components/Pagination";
@@ -18,11 +20,18 @@ const BreakingNews = ({ newList }: Props) => {
   const [paginationIndex, setPaginationIndex] = useState(0);
   const scrollX = useSharedValue(0);
   const ref = useAnimatedRef<Animated.FlatList<any>>();
+  const [ isAutoPlay, setIsAutoPlay ] = useState(true);
+  const interval = useRef<NodeJS.Timeout>();
+  const offset = useSharedValue(0);
+  const { width } = useWindowDimensions();
 
   const onScrollHandler = useAnimatedScrollHandler({
     onScroll: (e) => {
       scrollX.value = e.contentOffset.x;
     },
+    onMomentumEnd: (e) => {
+      offset.value = e.contentOffset.x;
+    }
   });
 
   const onViewableItemsChanged = ({
@@ -31,10 +40,27 @@ const BreakingNews = ({ newList }: Props) => {
     viewableItems: ViewToken[];
   }) => {
     if (viewableItems.length > 0 && viewableItems[0].index !== undefined) {
-      console.log("Visible item index:", viewableItems[0].index);
+      // console.log("Visible item index:", viewableItems[0].index); //Test slider item
       setPaginationIndex(viewableItems[0].index);
     }
   };
+
+  useEffect(() => {
+    if (isAutoPlay === true) {
+      interval.current = setInterval(() => {
+        offset.value = (offset.value + width) % (newList.length * width);
+      }, 5000);
+    } else {
+      clearInterval(interval.current);
+    }
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, [isAutoPlay, offset, width, newList.length]);
+
+  useDerivedValue(() => {
+    scrollTo(ref, offset.value, 0, true);
+  });
 
   const viewabilityConfig = {
     itemVisiblePercentThreshold: 50, 
@@ -61,6 +87,12 @@ const BreakingNews = ({ newList }: Props) => {
           onScroll={onScrollHandler}
           scrollEventThrottle={16}
           viewabilityConfigCallbackPairs={viewabilityConfigCallbackPairs.current}
+          onScrollBeginDrag={() => {
+            setIsAutoPlay(false);
+          }}
+          onScrollEndDrag={() => {
+            setIsAutoPlay(true);
+          }}
         />
         <Pagination
           items={newList}
