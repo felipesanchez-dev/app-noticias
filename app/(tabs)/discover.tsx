5df9940089afcd869e09fcb4153c91e8d48c8a9b
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-01-28 13:30:22 -0500

    Se añadió una barra de búsqueda (SearchBar) a la sección Discover Screen, sentando las bases para implementar su funcionalidad próximamente.

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import SearchBar from "@/components/SearchBar"
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const { top: safetop } = useSafeAreaInsets();
   return (
-    <View style={styles.container}>
-      <Text>Discover Screen</Text>
+    <View style={[styles.container, 
+    {paddingTop: safetop + 20}]}>
+      <SearchBar />
     </View>
   )
 }
@@ -16,7 +20,5 @@ export default Page
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 })
