5e7fc7a61f84dd9560014818f1055966fcd2b3b5
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-02-10 16:52:44 -0500

    Se agrego Politicas.hrml

--- html/Políticas de Privacidad.js
+++
@@ -1,4 +0,0 @@
-/* filepath: c:\Users\Pipedev\OneDrive\Documentos\app-noticias\script.js */
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Página de Políticas de Privacidad cargada correctamente.');
-});
